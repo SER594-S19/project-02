22d0d472699a54773911ceae3c6ac0d167687a74
nishitisawant <nnsawant@asu.edu>
2019-02-12 18:51:27 -0700

    Updating client GUI for multiple ports

--- Client/ClientDemo.java
+++ Client/ClientDemo.java
@@ -1,6 +1,6 @@
 package Client;
 
-import java.awt.BorderLayout;
+import java.awt.*;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -14,50 +14,172 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextArea;
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 
 public class ClientDemo extends JFrame implements Observer, ActionListener {
 
   private final Subscriber  [] subscriber = new Subscriber[2];
   private final ExecutorService service;
-  private JLabel iplabel = new JLabel("IP Address:");
-  private JLabel portlabel = new JLabel("Port Number:");
+
   //JTextField myTextField = new JTextField("Team Awesome!");
-  private JPanel panel = new JPanel(new BorderLayout());
-  private JPanel ipPanel = new JPanel(new BorderLayout());
-  private JPanel portPanel = new JPanel(new BorderLayout());
+
 
   private JTextArea textArea = new JTextArea();
-  private JTextArea ipNum = new JTextArea();
-  private JTextArea portNum = new JTextArea();
-  private JButton buttonConnect = new JButton("connect");
-  
+  private JTextArea ipNum1 = new JTextArea(1,10);
+  private JTextArea portNum1 = new JTextArea(1,5);
+  private JButton buttonConnect1 = new JButton("Connect");
+  private JTextArea ipNum2 = new JTextArea(1,10);
+  private JTextArea portNum2 = new JTextArea(1,5);
+  private JButton buttonConnect2 = new JButton("Connect");
+  private JTextArea ipNum3 = new JTextArea(1,10);
+  private JTextArea portNum3 = new JTextArea(1,5);
+  private JButton buttonConnect3 = new JButton("Connect");
+  private JTextArea ipNum4 = new JTextArea(1,10);
+  private JTextArea portNum4 = new JTextArea(1,5);
+  private JButton buttonConnect4 = new JButton("Connect");
+  private JTextArea ipNum5 = new JTextArea(1,10);
+  private JTextArea portNum5 = new JTextArea(1,5);
+  private JButton buttonConnect5 = new JButton("Connect");
+
   public ClientDemo() {
 
     service = Executors.newCachedThreadPool();
-    
+
     // TO TEST, RUN TWO SERVERS IN PORTS 1594 and 1595
-    
+
     //subscriber[0] = new Subscriber("localhost", 1594);
     //subscriber[1] = new Subscriber("localhost", 1595);
 
+    JLabel ipLabel1 = new JLabel("IP Address:");
+    JLabel portLabel1 = new JLabel("Port Number:");
+    JLabel ipLabel2 = new JLabel("IP Address:");
+    JLabel portLabel2 = new JLabel("Port Number:");
+    JLabel ipLabel3 = new JLabel("IP Address:");
+    JLabel portLabel3 = new JLabel("Port Number:");
+    JLabel ipLabel4 = new JLabel("IP Address:");
+    JLabel portLabel4 = new JLabel("Port Number:");
+    JLabel ipLabel5 = new JLabel("IP Address:");
+    JLabel portLabel5 = new JLabel("Port Number:");
+    JPanel topPanel= new JPanel();
+    topPanel.setLayout(new BoxLayout(topPanel, BoxLayout.Y_AXIS));
 
-    portNum.setBorder(BorderFactory.createLineBorder(Color.black));
-    ipNum.setBorder(BorderFactory.createLineBorder(Color.black));
+    JPanel inputPanel1 = new JPanel(new BorderLayout());
+    JPanel ipPanel1 = new JPanel(new BorderLayout());
+    JPanel portPanel1 = new JPanel(new BorderLayout());
+
+    //ipNum1.setLineWrap(true);
+    //ipNum1.setPreferredSize(new Dimension(10, 10));
+
+    portNum1.setBorder(BorderFactory.createLineBorder(Color.black));
+    ipNum1.setBorder(BorderFactory.createLineBorder(Color.black));
+
+    JPanel inputPanel2 = new JPanel(new BorderLayout());
+    JPanel ipPanel2 = new JPanel(new BorderLayout());
+    JPanel portPanel2 = new JPanel(new BorderLayout());
+
+    portNum2.setBorder(BorderFactory.createLineBorder(Color.black));
+    ipNum2.setBorder(BorderFactory.createLineBorder(Color.black));
+
+    JPanel inputPanel3 = new JPanel(new BorderLayout());
+    JPanel ipPanel3 = new JPanel(new BorderLayout());
+    JPanel portPanel3 = new JPanel(new BorderLayout());
+
+    portNum3.setBorder(BorderFactory.createLineBorder(Color.black));
+    ipNum3.setBorder(BorderFactory.createLineBorder(Color.black));
+
+    JPanel inputPanel4 = new JPanel(new BorderLayout());
+    JPanel ipPanel4 = new JPanel(new BorderLayout());
+    JPanel portPanel4 = new JPanel(new BorderLayout());
+
+    portNum4.setBorder(BorderFactory.createLineBorder(Color.black));
+    ipNum4.setBorder(BorderFactory.createLineBorder(Color.black));
+
+    JPanel inputPanel5 = new JPanel(new BorderLayout());
+    JPanel ipPanel5 = new JPanel(new BorderLayout());
+    JPanel portPanel5 = new JPanel(new BorderLayout());
+
+    portNum5.setBorder(BorderFactory.createLineBorder(Color.black));
+    ipNum5.setBorder(BorderFactory.createLineBorder(Color.black));
 
     setLayout(new BorderLayout());
 
-    ipPanel.add(iplabel,BorderLayout.WEST);
-    ipPanel.add(ipNum,BorderLayout.CENTER);
-    portPanel.add(portlabel,BorderLayout.WEST);
-    portPanel.add(portNum,BorderLayout.CENTER);
-    panel.add(ipPanel, BorderLayout.NORTH);
-    panel.add(portPanel, BorderLayout.CENTER);
+    ipPanel1.add(ipLabel1,BorderLayout.WEST);
+    ipPanel1.add(ipNum1,BorderLayout.CENTER);
+    ipPanel1.setBorder(new EmptyBorder(5, 5, 5, 5));
 
-    add(panel, BorderLayout.NORTH);
-    add(textArea, BorderLayout.CENTER);  
-    add(buttonConnect, BorderLayout.SOUTH);
+    portPanel1.add(portLabel1,BorderLayout.WEST);
+    portPanel1.add(portNum1,BorderLayout.CENTER);
+    portPanel1.setBorder(new EmptyBorder(5, 5, 5, 5));
 
-    buttonConnect.addActionListener(this);
+    ipPanel2.add(ipLabel2,BorderLayout.WEST);
+    ipPanel2.add(ipNum2,BorderLayout.CENTER);
+    ipPanel2.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+    portPanel2.add(portLabel2,BorderLayout.WEST);
+    portPanel2.add(portNum2,BorderLayout.CENTER);
+    portPanel2.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+    ipPanel3.add(ipLabel3,BorderLayout.WEST);
+    ipPanel3.add(ipNum3,BorderLayout.CENTER);
+    ipPanel3.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+    portPanel3.add(portLabel3,BorderLayout.WEST);
+    portPanel3.add(portNum3,BorderLayout.CENTER);
+    portPanel3.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+    ipPanel4.add(ipLabel4,BorderLayout.WEST);
+    ipPanel4.add(ipNum4,BorderLayout.CENTER);
+    ipPanel4.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+    portPanel4.add(portLabel4,BorderLayout.WEST);
+    portPanel4.add(portNum4,BorderLayout.CENTER);
+    portPanel4.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+    ipPanel5.add(ipLabel5,BorderLayout.WEST);
+    ipPanel5.add(ipNum5,BorderLayout.CENTER);
+    ipPanel5.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+    portPanel5.add(portLabel5,BorderLayout.WEST);
+    portPanel5.add(portNum5,BorderLayout.CENTER);
+    portPanel5.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+    //northPanel.add(ipPanel, BorderLayout.NORTH);
+    //northPanel.add(portPanel, BorderLayout.CENTER);
+    inputPanel1.setBorder(new EmptyBorder(5, 5, 5, 5));
+    inputPanel1.add(ipPanel1,BorderLayout.WEST);
+    inputPanel1.add(portPanel1,BorderLayout.CENTER);
+    inputPanel1.add(buttonConnect1,BorderLayout.EAST);
+
+    inputPanel2.setBorder(new EmptyBorder(5, 5, 5, 5));
+    inputPanel2.add(ipPanel2,BorderLayout.WEST);
+    inputPanel2.add(portPanel2,BorderLayout.CENTER);
+    inputPanel2.add(buttonConnect2,BorderLayout.EAST);
+
+    inputPanel3.setBorder(new EmptyBorder(5, 5, 5, 5));
+    inputPanel3.add(ipPanel3,BorderLayout.WEST);
+    inputPanel3.add(portPanel3,BorderLayout.CENTER);
+    inputPanel3.add(buttonConnect3,BorderLayout.EAST);
+
+    inputPanel4.setBorder(new EmptyBorder(5, 5, 5, 5));
+    inputPanel4.add(ipPanel4,BorderLayout.WEST);
+    inputPanel4.add(portPanel4,BorderLayout.CENTER);
+    inputPanel4.add(buttonConnect4,BorderLayout.EAST);
+
+    inputPanel5.setBorder(new EmptyBorder(5, 5, 5, 5));
+    inputPanel5.add(ipPanel5,BorderLayout.WEST);
+    inputPanel5.add(portPanel5,BorderLayout.CENTER);
+    inputPanel5.add(buttonConnect5,BorderLayout.EAST);
+
+    topPanel.add(inputPanel1);
+    topPanel.add(inputPanel2);
+    topPanel.add(inputPanel3);
+    topPanel.add(inputPanel4);
+    topPanel.add(inputPanel5);
+    add(topPanel, BorderLayout.NORTH);
+    add(textArea, BorderLayout.CENTER);
+    //add(buttonConnect, BorderLayout.SOUTH);
+
+    buttonConnect1.addActionListener(this);
     addWindowListener(new java.awt.event.WindowAdapter() {
       @Override
       public void windowClosing(java.awt.event.WindowEvent e) {
@@ -67,7 +189,7 @@ public class ClientDemo extends JFrame implements Observer, ActionListener {
     });
     setSize(500,500);
     setVisible(true);
-    
+
   }
 
   private void close() {
@@ -75,7 +197,7 @@ public class ClientDemo extends JFrame implements Observer, ActionListener {
     subscriber[0].stop();
     //subscriber[1].stop();
   }
-  
+
     private void shutdown() {
     subscriber[0].stop();
     //subscriber[1].stop();
@@ -87,7 +209,7 @@ public class ClientDemo extends JFrame implements Observer, ActionListener {
     } catch (InterruptedException ex) {
     }
   }
-  
+
 
   @Override
   public void update(Observable o, Object arg) {
@@ -96,30 +218,36 @@ public class ClientDemo extends JFrame implements Observer, ActionListener {
       textArea.append(data + "\n" );
     else {
       close();
-      buttonConnect.setEnabled(true);
-    }    
+      buttonConnect1.setEnabled(true);
+    }
   }
 
   public static void main(String[] args) {
     ClientDemo tester = new ClientDemo();
-     
+
   }
 
   @Override
   public void actionPerformed(ActionEvent e) {
+    buttonConnect1.setEnabled(false);
+    service.submit(subscriber[0]);
+    subscriber[0].addObserver(this);
+
+    service.submit(subscriber[1]);
+    subscriber[1].addObserver(this);
 	  int port = 0;
 	  try {
-		  port = Integer.parseInt(portNum.getText());
+		  port = Integer.parseInt(portNum1.getText());
 		  System.out.println("port from textbox = " + port);
 	  } catch (NumberFormatException ne) {
 		  System.out.println("number format exception");
 	  }
-	  String ip = ipNum.getText();
+	  String ip = ipNum1.getText();
 	  System.out.println("ipaddress from textbox = " + ip);
 	  for(int i=0;i<1;i++) {
 		  subscriber[i] = new Subscriber(ip, port);
 	  }
-	  buttonConnect.setEnabled(false);
+	  buttonConnect1.setEnabled(false);
 	  service.submit(subscriber[0]);
 	  subscriber[0].addObserver(this);
 
