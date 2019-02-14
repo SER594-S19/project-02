2a0e09d37c051292c92a62d52269d787b8c65e25
Manish Tandon <mtandon3@asu.edu>
2019-02-13 19:58:59 -0700

    minor change in to string methhod

--- HeartRate/HeartData.java
+++ HeartRate/HeartData.java
@@ -28,7 +28,7 @@ public class HeartData extends Core.Data{
   @Override
   public String toString() {
 	  
-	  return "time=" + this.time + ", value=" + values.get(0) ;
+	  return "Timestamp=" + this.time + ", Heart Rate=" + values.get(0) ;
    // return "Heart Rate = " + values.get(0);
   }
      
