3a9442b1decefdb6662e19c3c0d75f2b3f15df16
surya23 <surya.cherukuri@admitkard.com>
2019-02-17 23:50:17 -0700

    removed conflict

--- Client/ClientDemo.java
+++ Client/ClientDemo.java
@@ -1,134 +1,58 @@
 package Client;
 
-import java.awt.BasicStroke;
 import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
-
 import javax.swing.JButton;
-import javax.swing.JComboBox;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
 import javax.swing.JTextArea;
+import java.awt.FlowLayout;
 import javax.swing.JTextField;
-import javax.swing.Timer;
-
-import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartPanel;
-import org.jfree.chart.JFreeChart;
-import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.chart.plot.XYPlot;
-import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
-import org.jfree.data.xy.XYSeries;
-import org.jfree.data.xy.XYSeriesCollection;
-
-
 public class ClientDemo extends JFrame implements Observer, ActionListener {
 
-  private final Subscriber  [] subscriber;
+  private final Subscriber subscriber;
   private final ExecutorService service;
-  private JButton buttonConnect;
-  private JButton setGraph;
-  private JFreeChart chart;
-  private XYPlot plot;
-  private XYLineAndShapeRenderer renderer;
-  private ArrayList<JTextField> textFieldsList;
-  public static double graphData;
-  public static String graphLabel;
-
-  
+  private JTextArea textArea = new JTextArea();
+  private JButton buttonConnect; 
+  private final int PORT = 5914;
   JFrame frame;
   
-  public ClientDemo() {
+  
+public ClientDemo() {
 
     service = Executors.newCachedThreadPool();
-    
-    graphData = 0.0;
-    graphLabel = "Agreement";
-    renderer = new XYLineAndShapeRenderer();
-    textFieldsList = new ArrayList<JTextField>();
-    subscriber = new Subscriber[5];
-    
-    JPanel connectionPanel = new JPanel(new GridLayout(5, 0));
-    connectionPanel.setPreferredSize(new Dimension(100,100));
-    JPanel buttonPanel = new JPanel(new GridLayout(0,1));  
-    
-    JPanel graphPanel = new JPanel(new BorderLayout());
-    JPanel chartPanel = createGraphPanel();
-    graphPanel.add(chartPanel,BorderLayout.CENTER);
-    graphPanel.setVisible(true);
-	graphPanel.setPreferredSize(new Dimension(400,400));
-	    
-    JPanel dropDownPanel = new JPanel(new FlowLayout());
-	JComboBox serversList = new JComboBox();
-	JLabel dropDownLabel = new JLabel("Show in graph:");
-	//setGraph = new JButton("Display");
-	serversList.setPreferredSize(new Dimension(100,30));
-	dropDownPanel.setPreferredSize(new Dimension(70,70));
-	dropDownPanel.add(dropDownLabel);
-	dropDownPanel.add(serversList);
-	//dropDownPanel.add(setGraph);
-	graphPanel.add(dropDownPanel, BorderLayout.NORTH);
-	
-	serversList.addActionListener(new ActionListener() {
-		
-		@Override
-		public void actionPerformed(ActionEvent event) {
-			// TODO Auto-generated method stub
-			String selected = (String) ((JComboBox) event.getSource()).getSelectedItem();
-			for(int i=0;i<5;i++) {
-				if(null != subscriber[i])
-					subscriber[i].setServerPortSelected(selected);
-			}
-		}
-	});
-	
-    int i=1;
-     while(i<6) {    
-	     // create empty JTextField	  
-         buttonConnect= new JButton("CONNECT");
-	     JLabel label = new JLabel();		
-		 label.setText("ENTER PORT NUMBER "+i);
-		 
-		// create JTextField with default text
-	     JTextField field2 = new JTextField();
-	     textFieldsList.add(field2);
-	     JLabel label1 = new JLabel();		
-		 label1.setText("ENTER IP ADDRESS "+i);
-		 
-	     JTextField field1 = new JTextField();
-	     //field1.setText("IP Address");
-	     field1.setEnabled(false); 
-	     connectionPanel.add(label);
-	     connectionPanel.add(field2);
-	     connectionPanel.add(label1);
-	     connectionPanel.add(field1);
-	     connectionPanel.add(buttonConnect);
-	     
-	     serversList.addItem("Server Port "+i);
-	     
-	     buttonConnect.setVisible(false);
-	     i++;
-     }  
-     buttonConnect= new JButton("CONNECT");
-     add(graphPanel, BorderLayout.NORTH);
-     add(connectionPanel, BorderLayout.CENTER);
-     buttonPanel.add(buttonConnect);
-     //buttonPanel.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
-     add(buttonPanel,BorderLayout.SOUTH);
-     buttonConnect.addActionListener(this);
-     
+    subscriber = new Subscriber("localhost", PORT);
+    GridLayout gl = new GridLayout(5,3);
+   int i=1;
+    while(i<6) {    
+    // create empty JTextField
+    	buttonConnect= new JButton("connect");
+    JTextField field1 = new JTextField();
+    field1.setText("Ip address");
+
+    // create JTextField with default text
+    JTextField field2 = new JTextField("Port number");
+
+
+    add(field1);
+    add(field2);
+//    add(field3);
+//    add(field4);
+    field1.setEnabled(false);
+   // setLayout(new GridLayout(5,3));
+    //add(textArea, BorderLayout.CENTER);  
+   //add(buttonConnect, BorderLayout.EAST);  
+   add(buttonConnect);
+    buttonConnect.addActionListener(this);
+    i++;
+    }
+   
     addWindowListener(new java.awt.event.WindowAdapter() {
       @Override
       public void windowClosing(java.awt.event.WindowEvent e) {
@@ -136,60 +60,38 @@ public class ClientDemo extends JFrame implements Observer, ActionListener {
         System.exit(0);
       }
     });
-    
-    //this.getContentPane().setLayout(new GridLayout(5,1));
-    this.pack(); 
-    this.setTitle("Multimodal Client Connector");
-    this.setSize(900, 700);
-	this.setVisible(true);
-    
+    setSize(400,400);
+    setVisible(true);
+    this.getContentPane().setLayout(new GridLayout(5,3));
+    //frame.pack();
+	 
+	  frame.setVisible(true);
+	 
+	  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
   }
-  
-  private ChartPanel createGraphPanel() {
-      XYSeries series = new XYSeries("Data");
-     XYSeriesCollection dataset = new XYSeriesCollection(series);
-     new Timer(1000, new ActionListener() {
-
-         @Override
-         public void actionPerformed(ActionEvent e) {
-             series.add(series.getItemCount(), graphData);
-             chart.setTitle(graphLabel);
-         }
-     }).start();
-     chart = ChartFactory.createXYLineChart("Emotion", "Time",
-         "Range", dataset, PlotOrientation.VERTICAL, false, false, false);
-     plot = chart.getXYPlot();
-     renderer.setSeriesPaint(0, Color.GREEN);
-     renderer.setSeriesStroke(0, new BasicStroke(2.0f));
-     plot.setBackgroundPaint(Color.BLACK);
-     plot.setDomainGridlinesVisible(false);
-     plot.setRenderer(renderer);
-     
-     return new ChartPanel(chart) {
-		private static final long serialVersionUID = 4960544148862965383L;
-		@Override
-         public Dimension getPreferredSize() {
-             return new Dimension(480, 240);
-         }
-     };
- }
-    
-
+  private static void createAndShowGUI() {
+	  
+	  //Create and set up the window.
+	 
+//	  JFrame frame = new ClientDemo();
+//	 
+//	  //Display the window.
+//	 
+//	  frame.pack();
+//	 
+//	  frame.setVisible(true);
+//	 
+//	  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	 
+	    }
   private void close() {
     System.out.println("clossing ....... +++++++");
-    for(int i=0;i<5;i++)
-    {
-    	if(null != subscriber[i])
-    		subscriber[i].stop();
-    }
+    subscriber.stop();
   }
   
     private void shutdown() {
-    for(int i=0;i<5;i++)
-    {
-    	if(null != subscriber[i])
-    		subscriber[i].stop();
-    }
+    subscriber.stop();
     service.shutdown();
     try {
       if (!service.awaitTermination(10, TimeUnit.SECONDS)) {
@@ -203,40 +105,34 @@ public class ClientDemo extends JFrame implements Observer, ActionListener {
   @Override
   public void update(Observable o, Object arg) {
     String data = ((Subscriber) o).getObject().toString();
-    if (data.compareTo("FIN") != 0) {
-    	if(!data.isEmpty())
-    		graphData = Double.parseDouble(data.split(",")[1].split("=")[1].replace("}", ""));
-    	else
-    		graphData = 0.0;
-    	 System.out.println("Graph Data:"+graphData); 
-         System.out.println(data + "\n" );
-         //textArea.append();
-    }else {
+    if (data.compareTo("FIN") != 0)
+      textArea.append(data + "\n" );
+    else {
       close();
       buttonConnect.setEnabled(true);
     }    
   }
 
   public static void main(String[] args) {
-    new ClientDemo();  
+  // ClientDemo tester = 
+		   new ClientDemo();
+	  
+	  javax.swing.SwingUtilities.invokeLater(new Runnable() {
+		  
+		  public void run() {
+		   
+		      createAndShowGUI(); 
+		  }
+	   
+		    });
+     
   }
 
   @Override
   public void actionPerformed(ActionEvent e) {
-    int i = 0;
-    for(JTextField field : textFieldsList)
-    {
-    	if(!field.getText().isEmpty()) {
-    		if(null != subscriber[i]) 
-    			subscriber[i].stop();
-    	    subscriber[i] = new Subscriber("localhost", Integer.parseInt(field.getText()));
-    	    service.submit(subscriber[i]);
-    	    subscriber[i].setServerPortActive("Server Port "+(i+1));
-    	    subscriber[i].setServerPortSelected("Server Port 1");
-    	    subscriber[i].addObserver(this);
-    	}
-    	
-    	i++;
-    }
+    buttonConnect.setEnabled(false);
+    service.submit(subscriber);
+    subscriber.addObserver(this); 
+    
   }
 }
---
+++ Core/Gui.java
@@ -0,0 +1,293 @@
+package Core;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.awt.event.WindowEvent;
+import java.awt.image.BufferedImage;
+import java.awt.image.RescaleOp;
+import java.lang.*;
+
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.border.Border;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+public class Gui extends JPanel implements ActionListener, ChangeListener {
+
+  /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+private static Model model;
+  static String[] emotions = {"Agreement", "Concentrating", "Disagreement", "Interested", "Thinking", "Unsure"};
+  static JLabel[] labels = new JLabel[emotions.length];
+  private final int PORT = 1594;
+  protected JLabel labelPublishPort;
+  private final static JButton buttonConnect = new JButton("Connect");
+  static JFrame frame = new JFrame("Simulator");
+  static AlphaPanel[] ip = new AlphaPanel[6];
+  Dimension screen;
+  static Box outerBox;
+  static JPanel portNumber = new JPanel();
+  static JSlider[] slider = new JSlider[emotions.length];
+  static Box left = Box.createVerticalBox();
+  @SuppressWarnings("deprecation")
+private void createPanelSouth() {
+	  
+
+	  
+
+	    portNumber.setBackground(new Color(51,204,255));
+	    JLabel temp = new JLabel("  Publishing at port: ");
+	    System.out.println(frame.getHeight());
+	    System.out.println(frame.getWidth());
+	    temp.setFont(new Font("Serif", Font.PLAIN, 30));
+	    portNumber.add(temp);
+	    labelPublishPort = new JLabel("" + PORT);
+	    labelPublishPort.setFont(new Font("Serif", Font.PLAIN, 30));
+	    portNumber.add(labelPublishPort);
+	    portNumber.setPreferredSize(new Dimension(500, 50));
+	    portNumber.setMaximumSize(portNumber.getPreferredSize()); 
+	    portNumber.setMinimumSize(portNumber.getPreferredSize());
+	    Container content = frame.getContentPane();
+	    content.setLayout(new GridBagLayout());
+	    GridBagConstraints constraints = new GridBagConstraints();
+
+
+
+	    for(int i = 0;i<6;i++) {
+	    	 ImageIcon pic = createImageIcon(Integer.toString(i+1)+".png");
+	    	ip[i] = new AlphaPanel(pic, 0.75);
+	    }
+	    
+	    for(int i = 0; i < emotions.length; i++) {
+	    	labels[i] = new JLabel(emotions[i]);
+		    labels[i].setFont(new Font("Serif", Font.PLAIN, 30));
+		    labels[i].setHorizontalAlignment(JLabel.CENTER);
+	    }
+	    
+		
+		for(int i = 0; i < 6; i++) {
+			slider[i] = new JSlider(0, 10, 5);
+			slider[i].setMajorTickSpacing(1);
+			slider[i].setPaintTicks(true);
+			slider[i].setSize(800,400);
+			slider[i].setPaintLabels(true);
+			slider[i].setForeground(Color.BLUE);
+			slider[i].setBackground(new Color(204,204,204));
+			slider[i].addChangeListener(this);
+			//slider[i].setPreferredSize(new Dimension(500, 800));
+		}
+
+	    for(int i = 0; i < emotions.length; i++) {
+	    	
+	    	int t=i;
+	    	//constraints.fill = GridBagConstraints.BOTH;
+	    	
+	    	constraints.gridx = 0;
+	    	constraints.gridy = t;
+	    	constraints.gridwidth = 1;
+	    	constraints.gridheight = 1;
+	        //ip[i].setPreferredSize(new Dimension(100, 100));
+		    constraints.insets = new Insets(50,30,0,0);
+		   
+	    	constraints.gridwidth = 1;
+	    	constraints.fill = GridBagConstraints.HORIZONTAL;
+	    	constraints.anchor = GridBagConstraints.CENTER;
+	    	content.add(slider[i],constraints);
+	    	constraints.anchor = GridBagConstraints.NORTH;
+	    	 constraints.fill = GridBagConstraints.HORIZONTAL;
+		     content.add(labels[i],constraints);
+	    	//constraints.gridy = 2*t+1;
+	    	
+	    	
+	    }
+
+		 //addingSliders(t);
+	  	Insets insets = frame.getInsets();
+	  	int width = frame.getSize().width-(insets.left+insets.right);
+	  	int height = frame.getSize().height-(insets.top+insets.bottom);
+	  	
+	    //outerBox = Box.createHorizontalBox();
+	    //outerBox.add(left);
+	    //outerBox.setPreferredSize(new Dimension(600, 4600));
+	  	System.out.println(width+" "+height);
+	  	//left.setPreferredSize(new Dimension(600,4600));
+
+
+	      constraints.gridheight = 1;
+	      constraints.gridwidth = 1;
+	      constraints.gridx = 1;
+	      constraints.weightx = 1;
+	      constraints.weighty = 1;
+	      constraints.insets = new Insets(50,100,0,0);
+		    //content.add(ip,constraints);
+		    for(int i=0;i<6;i++) {
+		    	//constraints.insets = new Insets(-300+(i*150), 100, 0, 0);
+		    	constraints.anchor = GridBagConstraints.CENTER;
+		    	 constraints.gridx = 3;
+		    	 constraints.gridy = i;
+		        //ip[i].setPreferredSize(new Dimension(100, 100));
+		    	content.add(ip[i],constraints);
+		    }
+//		    
+
+	      constraints.gridx = 0;
+	      constraints.gridy = 6;   
+	    	 constraints.fill = GridBagConstraints.HORIZONTAL;
+	      content.add(portNumber, constraints);
+		    buttonConnect.setPreferredSize(new Dimension(200, 100));
+
+	     // constraints.insets = new Insets(100, -650,-200,-100);
+	      constraints.gridx = 3;
+	      constraints.gridy = 6; 
+	      constraints.insets = new Insets(50,100,0,50);
+		    content.add(buttonConnect, constraints);
+  }
+
+
+
+
+public Gui() {
+
+    model = new Model(new DataGenerator(), new Publisher(PORT));
+//    this.setBackground(Color.WHITE);
+//    this.setLayout(new BorderLayout());
+//    this.add(createPanelSouth(), BorderLayout.SOUTH);
+    screen = getToolkit().getScreenSize();
+//    frame.addComponentListener(new ComponentAdapter() {
+//        public void componentResized(ComponentEvent e) {
+//        	createPanelSouth();
+//        }
+//    });
+    //frame.setSize(new Dimension(screen.width/2, 3 * screen.height/4));
+    //frame.setLocation((screen.width - getSize().width) / 2, (screen.height - getSize().height) / 2);
+    frame.setBounds((screen.width)/5, (screen.height) /7, screen.width/3, 3 * screen.height/4);
+    //System.out.println(frame.getContentPane().getSize());
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    createPanelSouth();
+    System.out.println("gui done");
+  }
+
+  @Override
+  public void actionPerformed(ActionEvent e) {
+    System.out.println("listener trigger");
+    if (e.getSource() == buttonConnect) {
+      if (buttonConnect.getText().compareTo("run") == 0) {
+            System.out.println("start");
+        model.start();
+        buttonConnect.setText("stop");
+      } else if (buttonConnect.getText().compareTo("stop") == 0) {
+                    System.out.println("stop");
+        model.stop();
+        buttonConnect.setText("run");
+      }
+    }
+  }
+  
+  public void stateChanged(ChangeEvent ev)
+  {
+	  for(int i=0;i<6;i++) {
+		if(ev.getSource() == slider[i]) {
+		     int v = slider[i].getValue();
+		     System.out.println(v);
+             ip[i].setAlpha((float) v / slider[i].getMaximum());
+             ip[i].repaint();
+             break;
+      	}  
+	  }
+
+
+  }
+  
+
+  
+  public static void func()
+  {
+
+  }
+  
+  protected static ImageIcon createImageIcon(String path) {
+      java.net.URL gifURL = Gui.class.getResource(path);
+      if (gifURL != null) {
+          return new ImageIcon(gifURL);
+      } else {
+          System.err.println("Cannot find : " + path);
+          return null;
+      }
+  }
+  
+  public static void main(String[] args) {
+    frame.add(new Gui());
+	Insets insets = frame.getInsets();
+  	int width = frame.getSize().width-(insets.left+insets.right);
+  	int height = frame.getSize().height-(insets.top+insets.bottom);
+    System.out.println(width+"yo"+height);
+    //outerBox.setPreferredSize(new Dimension(width/2, 3*height/4));
+
+
+    //frame.pack();
+    //frame.setSize(800, 1000);
+    frame.setVisible(true);
+
+  }
+  
+}
+
+
+class AlphaPanel extends JPanel {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private BufferedImage bi;
+    private float[] scales = {1f, 1f, 1f, 0.5f};
+    private float[] offsets = new float[4];
+    private RescaleOp rop;
+
+    public AlphaPanel(ImageIcon icon, double scale) {
+
+        int width = (int) (scale * icon.getIconWidth());
+        int height = (int) (scale * icon.getIconHeight());
+        this.setPreferredSize(new Dimension(width, height));
+        this.bi = new BufferedImage(
+            width, height, BufferedImage.TYPE_INT_ARGB);
+        this.bi.createGraphics().drawImage(
+            icon.getImage(), 0, 0, width, height, null);
+        rop = new RescaleOp(scales, offsets, null);
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.drawImage(bi, rop, 0, 0);
+    }
+
+    public void setAlpha(float alpha) {
+        this.scales[3] = alpha;
+        this.rop = new RescaleOp(scales, offsets, null);
+    }
+}
